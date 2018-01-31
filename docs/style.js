c4843d9adfb76bc0766ea949ea9ff5a36b90be80
KAN <kabirajitnagral@gmail.com>
2018-01-31 12:59:46 -0500

    Testing stuff with the website menu

--- docs/style.js
+++ docs/style.js
@@ -16,8 +16,19 @@ function onClick(element) {
   captionText.innerHTML = element.alt;
 }
 
-function myAccFunc(element) {
-    var x = document.getElementById(element);
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+function myAccFunc1() {
+    var x = document.getElementById("Acc");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
         x.previousElementSibling.className += " w3-green";

d81676fa44f27b1e658b35bba56169a86e05d941
KAN <kabirajitnagral@gmail.com>
2018-01-31 23:04:48 -0500

    Testing some stuff for the website

--- docs/style.js
+++ docs/style.js
@@ -27,25 +27,25 @@ function myAccFunc(element) {
         x.previousElementSibling.className.replace(" w3-green", "");
     }
 }
-function myAccFunc1() {
-    var x = document.getElementById("Acc");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-green";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-        x.previousElementSibling.className.replace(" w3-green", "");
-    }
-}
-function myAccFunc2() {
-    var x = document.getElementById("Acc1");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-        x.previousElementSibling.className += " w3-green";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-        x.previousElementSibling.className =
-        x.previousElementSibling.className.replace(" w3-green", "");
-    }
-}
+// function myAccFunc1() {
+//     var x = document.getElementById("Acc");
+//     if (x.className.indexOf("w3-show") == -1) {
+//         x.className += " w3-show";
+//         x.previousElementSibling.className += " w3-green";
+//     } else {
+//         x.className = x.className.replace(" w3-show", "");
+//         x.previousElementSibling.className =
+//         x.previousElementSibling.className.replace(" w3-green", "");
+//     }
+// }
+// function myAccFunc2() {
+//     var x = document.getElementById("Acc1");
+//     if (x.className.indexOf("w3-show") == -1) {
+//         x.className += " w3-show";
+//         x.previousElementSibling.className += " w3-green";
+//     } else {
+//         x.className = x.className.replace(" w3-show", "");
+//         x.previousElementSibling.className =
+//         x.previousElementSibling.className.replace(" w3-green", "");
+//     }
+// }
